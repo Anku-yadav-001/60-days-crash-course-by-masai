dbe94efcd0236201caa06e25ff883e8eebac0d1e
Anku-yadav-001 <yaman49751@gmail.com>
2024-05-25 11:58:14 +0530

    done

--- react-final-assignment/question1/src/Pages/About.jsx
+++ react-final-assignment/question1/src/Pages/About.jsx
@@ -1,7 +1,87 @@
+import {Box, Container, Heading, SimpleGrid} from "@chakra-ui/react"
 
 export function About(){
-
+   
     return <>
-      <h1>About Page</h1>
+       <Box>
+            <Container maxW='50%' height="50vh" textAlign="center" p="20px" borderRadius="10px" mt="20px" background="orange.400" color="white">
+              <Heading as="h3" size="lg">We help cannabies clubs find real estate</Heading>
+              <p>Found by a former city planner and a connabies industry pioneer. Our team has decades of experience finding and permitting peroperties for cannonies businesses. We understand the unique challanges and opportunities of the cannabies industry, and we're here to help you succed.</p>
+            
+            </Container>
+           <Container maxW="50%" mt="20px">
+               <Heading as="h6" size="md" textAlign="left">About</Heading>
+			<p style={{fontSize:"14px"}}>Founded in 2010, European Grains has emerged as one of Europe's leading grain and oilseed trading companies. Our business is based on our strong relationships with producers and consumers and our ability to supply high-quality products. We have built a reputation for relputation for reliability, integrity, and innovation, we are commited to helping our customers succeed. Our mission is to be most tructed partner for our customers and suppliers, providing them eith the best products and service. We are dedicated to building long-term relationships and creating value for all stackholders. We belive in the importance of sustaninability and are commited to ensuring that our business practices are environmentally and socially responsible. We are constnously working to imporve our operations our operations and reducing our impact on the environment.</p>
+           </Container>
+       </Box> 
+       <Container maxW="50%" mt="20px">
+       <Heading as="h6" size="md" textAlign="left" p="10px">Our Products</Heading>
+       <SimpleGrid columns={5} spacing={10}>
+           <Box height="100px" textAlign="center">
+         <img src="https://images4.alphacoders.com/764/thumb-1920-764670.jpg" alt="" style={{height:"100%",width:"100%", borderRadius:"10px"}}/>
+         <p>Wheat</p>
+           </Box>
+           <Box height="100px" textAlign="center">
+              <img src="https://th.bing.com/th/id/OIP.K4mRBm2u3KxmoqkDonMrIwHaHa?rs=1&pid=ImgDetMain" alt="" style={{height:"100%",width:"100%", borderRadius:"10px"}}/>
+              <p>Rye</p>
+           </Box>
+           <Box height="100px" textAlign="center">
+            <img src="https://th.bing.com/th/id/OIP.gL1y7TtNhchgsN4E9RyCuQHaE8?rs=1&pid=ImgDetMain" alt="" style={{height:"100%",width:"100%", borderRadius:"10px"}}/>
+            <p>Barley</p>
+           </Box>
+           <Box height="100px" textAlign="center">
+            <img src="https://www.healthbenefitstimes.com/glossary/wp-content/uploads/2020/10/Maize.jpg" alt="" style={{height:"100%",width:"100%", borderRadius:"10px"}}/>
+            <p>Maize</p>
+           </Box>
+           <Box height="100px" textAlign="center">
+            <img src="https://th.bing.com/th/id/OIP.W6JL-RGczAzMk8OGS4xcowHaGE?rs=1&pid=ImgDetMain" alt="" style={{height:"100%",width:"100%", borderRadius:"10px"}}/>
+            <p>OilSeeds</p>
+           </Box>
+           <Box height="100px" textAlign="center">
+            <img src="https://th.bing.com/th/id/OIP.W6JL-RGczAzMk8OGS4xcowHaGE?rs=1&pid=ImgDetMain" alt="" style={{height:"100%",width:"100%", borderRadius:"10px"}}/>
+            <p>Wheat Flour</p>
+           </Box>
+           <Box height="100px" textAlign="center">
+            <img src="https://th.bing.com/th/id/OIP.O4GJdvmwdoHCtw0oJVN1LAHaE0?rs=1&pid=ImgDetMain" alt="" style={{height:"100%",width:"100%", borderRadius:"10px"}}/>
+            <p>Wheat Bran</p>
+           </Box>
+           <Box height="100px" textAlign="center">
+            <img src="https://th.bing.com/th/id/OIP.7k3MlSzWsp_wAgCTLDb86QAAAA?rs=1&pid=ImgDetMain" alt="" style={{height:"100%",width:"100%", borderRadius:"10px"}}/>
+            <p>Wheat Grem</p>
+           </Box>
+           <Box height="100px" textAlign="center">
+            <img src="https://th.bing.com/th/id/OIP.MeUTh-MoTHAxGFONjDn_HQHaFD?rs=1&pid=ImgDetMain" alt="" style={{height:"100%",width:"100%", borderRadius:"10px"}}/>
+            <p>Ray Flour</p>
+           </Box>
+           <Box height="100px" textAlign="center">
+            <img src="https://th.bing.com/th/id/OIP.DNj6efnAEGQEAyJs7STAjgHaF7?rs=1&pid=ImgDetMain" alt="" style={{height:"100%",width:"100%", borderRadius:"10px"}}/>
+            <p>Barley Flour</p>
+           </Box>
+           <Box height="100px" textAlign="center">
+            <img src="https://th.bing.com/th/id/OIP.K66YxS-0AAUU-2iz9EqkxgHaE7?rs=1&pid=ImgDetMain" alt="" style={{height:"100%",width:"100%", borderRadius:"10px"}}/>
+            <p>Maize Flour</p>
+           </Box>
+           <Box height="100px" textAlign="center">
+            <img src="https://th.bing.com/th/id/OIP.phy71tG3QoDmFQCPGJSdmwHaHh?rs=1&pid=ImgDetMain" alt="" style={{height:"100%",width:"100%", borderRadius:"10px"}}/>
+            <p>Maize Meal</p>
+           </Box>
+           <Box height="100px" textAlign="center">
+            <img src="https://th.bing.com/th/id/OIP.OJsyXjmhdjQOjGX5l_ORxgHaIC?rs=1&pid=ImgDetMain" alt="" style={{height:"100%",width:"100%", borderRadius:"10px"}}/>
+            <p>Sunflower Cake</p>
+           </Box>
+           <Box height="100px" textAlign="center">
+            <img src="https://th.bing.com/th/id/OIP.LJgRvTFpUPDBCWUEeP-68AHaHa?rs=1&pid=ImgDetMain" alt="" style={{height:"100%",width:"100%", borderRadius:"10px"}}/>
+            <p>Rapeseed Cake</p>
+           </Box>
+           <Box height="100px" textAlign="center">
+            <img src="https://th.bing.com/th/id/OIP.qgqPv-zWFu_xYQ6Ojv4QXwHaE9?rs=1&pid=ImgDetMain" alt="" style={{height:"100%",width:"100%", borderRadius:"10px"}}/>
+            <p>Soyabean Meal</p>
+           </Box>
+           <Box height="100px" textAlign="center">
+            <img src="https://th.bing.com/th/id/OIP.khxYfkjDEJUY3ejIwGbneAHaE8?rs=1&pid=ImgDetMain" alt="" style={{height:"100%",width:"100%", borderRadius:"10px"}}/>
+            <p>Soyabean Hulls</p>
+           </Box>
+       </SimpleGrid>
+       </Container>
     </>
 }
--- react-final-assignment/question1/src/Pages/Home.jsx
+++ react-final-assignment/question1/src/Pages/Home.jsx
@@ -1,13 +1,46 @@
 import {Box, Container, Heading} from "@chakra-ui/react"
+import "../Pages/My.css"
 export function Home(){
 
     return <>
-       <Box>
+       {/* <Box>
             <Container border="2px solid red" maxW='70%'>
               <img src="https://i.pinimg.com/originals/0a/7e/eb/0a7eeb41ca4c7d8495c73891ec784e52.jpg" alt="" style={{width:"100%",height:"70vh"}}/>
               <Heading as="h3" size="lg" mt="-30%">We help cannabies clubs find real estate</Heading>
               <p>Found by a former city planner and a connabies industry pioneer. Our team has decades of experience finding and permitting peroperties for cannonies businesses. We understand the unique challanges and opportunities of the cannabies industry, and we're here to help you succed.</p>
             </Container>
-       </Box>
+       </Box> */}
+<div class="container">
+		<div class="header">
+			<h1>Cannabis Club Real Estate</h1>
+		</div>
+		<div class="content">
+			<div class="section">
+				<h2>We help cannabis clubs find real estate</h2>
+				<p>Founded by a former city planner and a cannabis industry pioneer, our team has decades of experience finding and permitting properties for cannabis businesses. We understand the unique challenges and opportunities of the cannabis industry, and we're here to help you succeed.</p>
+				<a href="#" class="button">Learn more</a>
+			</div>
+			<div class="section">
+				<h2>How we help</h2>
+				<ul>
+					<li>Find the perfect location for your cannabis club</li>
+					<li>Work with experienced consultants</li>
+					<li>Get help with permits</li>
+					<li>Navigate the complex web of regulations and permits</li>
+					<li>Provide guidance on everything from branding to compliance</li>
+				</ul>
+				<a href="#" class="button">Get a free consultation</a>
+			</div>
+			<div class="section">
+				<h2>Trusted by top cannabis brands</h2>
+				<p>We've helped countless cannabis clubs find their perfect location and navigate the complex web of regulations and permits. Here's what some of our clients have to say:</p>
+				<blockquote>
+					<p>"Great service! The team really knows their stuff and helped us find the perfect location for our cannabis club." - Jane</p>
+					<p>"Highly recommend! They made the permit process a breeze and provided valuable insights on branding and compliance." - John</p>
+					<p>"Excellent experience from start to finish. They're a trusted partner for anyone looking to start or expand a cannabis business." - Jack</p>
+				</blockquote>
+        </div>
+        </div>
+        </div>
     </>
 }
